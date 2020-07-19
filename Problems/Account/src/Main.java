0948b2716aa930a156ae34d52c07e7bb97792182
Melanie <mmunk1994@gmail.com>
2020-07-19 17:29:54 -0600

    basic constructor problems

---
+++ Problems/Account/src/Main.java
@@ -0,0 +1,11 @@
+class Account {
+    long balance;
+    String ownerName;
+    boolean locked;
+
+    public Account(long balance, String ownerName, boolean locked) {
+        this.balance = balance;
+        this.ownerName = ownerName;
+        this.locked = locked;
+    }
+}
